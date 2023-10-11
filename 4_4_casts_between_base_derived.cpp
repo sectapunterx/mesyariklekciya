571252c524c75e11b22ca70d22b14d6ad4d5b614
Anton <azoto033@gmail.com>
2023-10-11 22:24:12 +0300

    4.4 Casts between base and derived classes. cherry picked

--- 4_4_casts_between_base_derived.cpp
+++ 4_4_casts_between_base_derived.cpp
@@ -15,7 +15,6 @@ public:
 };
 
 class Derived: public Base {
-    //TODO: Приватное наследование означает, что мы создаем наследника, но он не дает другим считать себя наследником
 public:
     int b = 1;
     Derived() = default;
@@ -24,14 +23,23 @@ public:
     }
 };
 
+void g(Derived& d){
+
+}
+
+void f(Base& b){
+    //g(b); // error: can't convert Base* to Derived*
+}
+
 
 int main() {
     Derived d;
-    Base& b = d;
-    static_cast<Derived&>(b); // OK: Derived is derived from Base
-    static_cast<Base&>(d); // OK: Derived is derived from Base
 
     // slicing
     Base* pb = &d; // OK: Derived is derived from Base
-
+    //Derived* pd = pb; //Cannot initialize a variable of type 'Derived *' with an lvalue of type 'Base *'
+    
+    //Base& br = d; // OK: Derived is derived from Base
+    //Derived& dd = br; //error: can't convert Base* to Derived*
+    //f(d); // OK: Derived is derived from Base
 }
