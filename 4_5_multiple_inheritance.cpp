0d53f0b699b5c873345967d37cdfc769c1a45c5d
Anton <azoto033@gmail.com>
2023-10-11 23:22:12 +0300

    4.5 Multiple Inheritance (SPROSIT' U GPT ZACHEM NUJEN POINTER NA OBJECT DRUGOGO CLASSA!!!!!!)

--- 4_5_multiple_inheritance.cpp
+++ 4_5_multiple_inheritance.cpp
@@ -1,3 +1,25 @@
-//
 // Created by Fin on 11.10.2023.
-//
+//4.5 Multiple inheritance.
+
+#include <iostream>
+
+struct Mother {
+    int m = 1;
+};
+
+struct Father {
+    int f = 2;
+};
+
+struct Son: public Mother, public Father {
+    int s = 3;
+};
+
+int main(){
+    Son s;
+    std::cout << &s << std::endl;
+
+    Father* pf = &s;
+
+    std::cout << pf << std::endl;
+}
