fedfbeeffe98561ff018f530799734a204e622da
Anton <azoto033@gmail.com>
2023-11-07 15:29:27 +0300

    6.6 Dependent names problem

---
+++ 6_6_dependent_names_problem.cpp
@@ -0,0 +1,44 @@
+//
+// Created by Fin on 01.11.2023.
+//6.6 Dependent names problem
+#include "iostream"
+
+template <typename T>
+struct S{
+    using X = T;
+};
+
+template<>
+struct S<int> {
+    static int X;
+};
+
+//int a = 0;
+
+template <typename T>
+void f(){
+    typename S<T>::X * a;
+}
+
+template <typename T>
+struct SS{
+    template<int M, int N>
+    struct A{
+
+    };
+};
+
+template<>
+struct SS<int> {
+    static const int A;
+};
+
+template <typename T>
+void g(){
+    typename SS<T>::template A<1, 2> a;
+}
+
+int main(){
+    /*f<int>();
+    f<double>();*/
+}
