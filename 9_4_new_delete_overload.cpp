c9a73649e2048b496202aa1cc63aca1d79537b6d
Anton <azoto033@gmail.com>
2023-12-18 23:14:59 +0300

    9.4 New & Delete overloading

---
+++ 9_4_new_delete_overload.cpp
@@ -0,0 +1,25 @@
+// Created by Fin on 18.12.2023.
+//9.4 new and delete overload
+#include "iostream"
+#include "vector"
+#include "cstdlib"
+
+void* operator new(size_t n){
+  std::cout << "Allocating " << n << " bytes\n";
+  void* ptr = std::malloc(n);
+  if(!ptr){
+    throw std::bad_alloc();
+  }
+  return ptr;
+}
+
+void operator delete(void* ptr) noexcept{
+  std::free(ptr);
+}
+
+int main(){
+  std::vector<int> v(5);
+  for(int i = 6; i < 10; ++i){
+    v.push_back(i);
+  }
+}
