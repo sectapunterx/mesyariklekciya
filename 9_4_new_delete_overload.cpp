86c19e4ce8010db30649fe84986198f63210000a
Anton <azoto033@gmail.com>
2023-12-20 23:33:28 +0300

    9.4 New Delete overloading

--- 9_4_new_delete_overload.cpp
+++ 9_4_new_delete_overload.cpp
@@ -13,14 +13,47 @@ void* operator new(size_t n){
   return ptr;
 }
 
+void* operator new(size_t n, const std::nothrow_t&) noexcept{
+  std::cout << "Allocating " << n << " bytes non-throwing\n";
+  void* ptr = std::malloc(n);
+  return ptr;
+}
+
+//PLACEMENT NEW NOT OVERLOADABLE
+/*void* operator new(size_t n, void* ptr) noexcept{
+  std::cout << "Placement new" << n << " bytes\n";
+  return ptr;
+}*/
+
 void operator delete(void* ptr) noexcept{
   std::cout << "Deallocating\n";
   std::free(ptr);
 }
 
+struct S{
+    static void* operator new(size_t n){
+      std::cout << "Allocating " << n << " bytes for S\n";
+      void* ptr = std::malloc(n);
+      if(!ptr){
+        throw std::bad_alloc();
+      }
+      return ptr;
+    }
+
+    static void operator delete(void* ptr) noexcept{
+      std::cout << "Deallocating for S\n";
+      std::free(ptr);
+    }
+};
+
 int main(){
-  std::vector<int> v(5);
+ /* int* p = new(std::nothrow) int(5);
+  delete p;*/
+  S* p = new S();
+  delete p;
+
+  std::vector<S> v(5);
   for(int i = 6; i < 100; ++i){
-    v.push_back(i);
+    v.push_back(S());
   }
 }
