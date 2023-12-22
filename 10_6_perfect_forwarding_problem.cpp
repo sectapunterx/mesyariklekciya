183101a69a26eb05564eff187a44ad5790c89220
Anton <azoto033@gmail.com>
2023-12-22 22:12:03 +0300

    10.6 Perfect Forwarding + Universal Reference

--- 10_6_perfect_forwarding_problem.cpp
+++ 10_6_perfect_forwarding_problem.cpp
@@ -1,3 +1,27 @@
 //
 // Created by Fin on 22.12.2023.
 //
+#include "iostream"
+
+template<typename T, typename Alloc = std::allocator<T>>
+class vector {
+private:
+    T* arr;
+    size_t sz;
+    size_t cap;
+    Alloc alloc;
+
+    using AllocatorTraits = std::allocator_traits<Alloc>;
+public:
+    void reserve(int);
+
+    template<typename ... Args>
+    void emplace_back(Args&& ... args) { //universal reference
+      if (sz >= cap) {
+        reserve(cap * 2);
+      }
+      AllocatorTraits::construct(alloc, arr + sz, std::forward<Args>(args)...); //placement new
+      ++sz;
+    }
+
+};
