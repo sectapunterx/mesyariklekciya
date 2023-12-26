9d63b7ba18d4cfcf81addc1f0e842bc382c1b37f
Anton <azoto033@gmail.com>
2023-12-26 18:28:30 +0300

    10.8 Copy elision

---
+++ 10_8_copy_elision.cpp
@@ -0,0 +1,27 @@
+#include "iostream"
+#include "vector"
+
+//10.8 Copy elision
+
+struct S{
+    int x;
+    S(int x): x(x){
+      std::cout << x << std::endl;
+    }
+    S(const S&){
+      std::cout << 1 << std::endl;
+    }
+};
+
+S f(int n){
+  return S(n);
+}
+
+int main(){
+  S v = f(10); // copy constructor 0 times -- copy elision
+
+  S s = S(S(S(S(S(5))))); // copy constructor 0 times -- copy elision
+
+  int x = f(10).x;
+}
+
